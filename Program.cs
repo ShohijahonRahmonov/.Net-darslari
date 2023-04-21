6bafe10eb6629eb6b90f1b3a33685bfb48892bf4
Shohijahon Rahmonov <shohijahonrahmonov2023@gmail.com>
2023-04-21 15:44:07 +0500

    Matematik formulalar bor

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Math 
+
+// Bu kod ikki soning kattasini aniqlaydi.
+Console.WriteLine ($"Katta son: {Math.Max(33,41)}"); 
+Console.WriteLine ($"Katta son: {Math.Max(33,41)}"); 
+
+// Bu kod ikki sonning kichigini aniqlaydi.
+Console.WriteLine ($"Kichik son {Math.Min(33, 41)}");
+Console.WriteLine ($"Kichik son {Math.Min (23.4, -56.7)}");
+
+// Bu kod sonni kvadrad ildizdan  chiqaradi.
+Console.WriteLine($"kvadrat ildizdan chiqaradi {Math.Sqrt(81)}");
+Console.WriteLine($"kvadrat ildizdan chiqaradi {Math.Sqrt(64)}");
+
+// Bu kod manfiy soni musbatga aylantiradi. 
+Console.WriteLine (Math.Abs(-375));
+Console.WriteLine (Math.Abs(-23.3));
+
+// Bu kod soni eng yaqin butun songa yaxlitlaydi.
+Console.WriteLine(Math.Round(55.4));
+Console.WriteLine(Math.Round(55.8));
+
+
