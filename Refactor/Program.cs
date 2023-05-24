2bb6263c8f8b301b45138ba8b09e496ee59146d1
Shohijahon Rahmonov <shohijahonrahmonov2023@gmail.com>
2023-05-24 19:46:15 +0500

    Refactor

---
+++ Refactor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Calculate 
+{
+    class Calculator
+    {
+        static void Main (string [] args)
+        {
+            Console.Write("Enter you name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter you surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("How old are you: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Where are you from: ");
+            string town =  Console.ReadLine ();
+           
+            Console.WriteLine ($"{name} {surname} {age} yoshda\n{name} {town} shahrida yashaydi.");
+
+        }
+    }
+}
